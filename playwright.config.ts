3935e66592678caf76f64f7f4757b622bda43656
ANKM0 <ANKM0@users.noreply.github.com>
2026-08-22 00:24:42 +0900

    fix(ci): #165 satisfy strict environment access

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   webServer: {
     command: "pnpm vite --host 127.0.0.1 --port 4173",
     port: 4173,
-    reuseExistingServer: !process.env.CI
+    reuseExistingServer: !process.env["CI"]
   },
   projects: [
     {
